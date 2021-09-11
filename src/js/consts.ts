4c2891a3589b803a4121551c664a1d3abba7ff6e
Roberto Capuano <roberto.capuano@icloud.com>
2021-09-11 07:26:29 +0200

    v1.1.0

--- src/js/consts.ts
+++ src/js/consts.ts
@@ -9,7 +9,7 @@ export const PLANET_W = 150;
 export const SHORT_NOTE = 200;
 export const LONG_NOTE = 300;
 
-export const VERSION = '1.0.0'
+export const VERSION = '1.1.0'
 
 export const CARGO_ITEM = 'guild_cargo';
 export const SYSTEMS_ITEM = 'guild_systems';
